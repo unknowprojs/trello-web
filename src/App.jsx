4b56815e69e1147da02b9b320c8c8846ec36b99a
khaikieu1407 <kieuquangkhai.pt@gmail.com>
2023-07-15 18:22:01 +0700

    add custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <h1>q2kdev 1k$</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione unde voluptatibus hic sed tenetur dolorem deleniti esse id harum nesciunt sequi veritatis maiores maxime libero laboriosam, blanditiis, laudantium ad. Modi.git</p>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
