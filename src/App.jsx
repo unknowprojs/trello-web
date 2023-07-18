ed5897590243f8d6ac497c309d5ec285da0823c1
khaikieu1407 <kieuquangkhai.pt@gmail.com>
2023-07-18 23:12:04 +0700

    custom mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,39 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import { pink } from '@mui/material/colors'
+import theme from './theme'
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
-      <h1>q2kdev 1k$</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione unde voluptatibus hic sed tenetur dolorem deleniti esse id harum nesciunt sequi veritatis maiores maxime libero laboriosam, blanditiis, laudantium ad. Modi.git</p>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon sx={{ color: pink[100] }}/>
-      <ThreeDRotation />
+      <CssVarsProvider theme={theme} >
+        <ModeToggle />
+        <h1>q2kdev 1k$</h1>
+        <p>Fe developergit</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione unde voluptatibus hic sed tenetur dolorem deleniti esse id harum nesciunt sequi veritatis maiores maxime libero laboriosam, blanditiis, laudantium ad. Modi.git</p>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <AccessAlarmIcon sx={{ color: pink[100] }}/>
+        <ThreeDRotation />
+      </CssVarsProvider>
     </>
   )
 }
